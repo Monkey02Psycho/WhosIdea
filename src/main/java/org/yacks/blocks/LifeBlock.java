e6ecadefa943baced8eafb6242b4a45463880126
Monkey02Psycho <monkeypsycho03@gmail.com>
2020-12-03 12:58:47 -0700

    added docs to LifeBlock, made sure it requires tool

--- src/main/java/org/yacks/blocks/LifeBlock.java
+++ src/main/java/org/yacks/blocks/LifeBlock.java
@@ -10,7 +10,9 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
-
+/**
+ * A block when a player collides with it will heal 2 hearts
+ */
 public class LifeBlock extends Block {
 
     public LifeBlock() {
@@ -18,9 +20,13 @@ public class LifeBlock extends Block {
                 .harvestTool(ToolType.PICKAXE).setRequiresTool());
     }
 
+    /**
+     * checks if entity is player, if so heal 2 hearts
+     */
     @Override
     public void onEntityCollision(BlockState state, World worldIn, BlockPos pos, Entity entityIn) {
         if (entityIn instanceof PlayerEntity) {
+            // healing 4 because 1 is actually half a heart
             ((LivingEntity) entityIn).heal(4);
         }
     }
