4ca44f22841e883fb3828d50bb27ec1da4517057
Monkey02Psycho <monkeypsycho03@gmail.com>
2020-12-03 16:39:45 -0700

    Fixed Solidified fire related bugs
    Solidified fire messages now work on server, texture should render, name should work

--- src/main/java/org/yacks/items/SolidifiedFire.java
+++ src/main/java/org/yacks/items/SolidifiedFire.java
@@ -12,23 +12,28 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
+
 /**
- * an item that plays random messages when you hold it.
- * Im well aware there is no logic in having solid fire but your in a world where holding
- * 6082560 kilograms is just as easy as holding 1856 feathers.
+ * an item that plays random messages when you hold it. Im well aware there is
+ * no logic in having solid fire but your in a world where holding 6082560
+ * kilograms is just as easy as holding 1856 feathers.
  */
 public class SolidifiedFire extends Item {
 
     /**
      * A list of all the possible chat messages
-     * @see net.minecraft.client.resources.I18n
+     * 
+     * @see net.minecraft.util.text.TranslationTextComponent
      */
-    private String[] messages = { I18n.format("messages.whosidea.solid_fire.1"),
-            I18n.format("messages.whosidea.solid_fire.2"), I18n.format("messages.whosidea.solid_fire.3"),
-            I18n.format("messages.whosidea.solid_fire.4"), I18n.format("messages.whosidea.solid_fire.5") };
+    private ITextComponent[] messages = { new TranslationTextComponent("messages.whosidea.solid_fire.1"),
+            new TranslationTextComponent("messages.whosidea.solid_fire.2"),
+            new TranslationTextComponent("messages.whosidea.solid_fire.3"),
+            new TranslationTextComponent("messages.whosidea.solid_fire.4"),
+            new TranslationTextComponent("messages.whosidea.solid_fire.5") };
 
     // Random instance for random messages
     Random random = new Random();
@@ -39,17 +44,19 @@ public class SolidifiedFire extends Item {
     }
 
     /**
-     * called every tick its in the players inventory, sets player on fire and if its held play a random chat message
+     * called every tick its in the players inventory, sets player on fire and if
+     * its held play a random chat message
      */
     @Override
     public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
         // sets the entity that has it in inventory on fire for 15 ticks
         entityIn.setFire(15);
         /*
-        if a randomly made number is 0 and the item is selected then It will put a random message in chat
-        */
+         * if a randomly made number is 0 and the item is selected then It will put a
+         * random message in chat
+         */
         if (isSelected && random.nextInt(100) == 0) {
-            entityIn.sendMessage(ITextComponent.getTextComponentOrEmpty(random_message()), entityIn.getUniqueID());
+            entityIn.sendMessage(random_message(), entityIn.getUniqueID());
         }
 
     }
@@ -64,7 +71,7 @@ public class SolidifiedFire extends Item {
     /**
      * @return any random from the messages variable
      */
-    private String random_message() {
+    private ITextComponent random_message() {
         return messages[random.nextInt(messages.length)];
     }
 }
